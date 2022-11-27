450925c54913ba68b26a64a95dbd30f0b0d48338
Wesin Ribeiro <wesinec@gmail.com>
2022-11-27 17:48:29 -0300

    formata minimos quadrados

--- aprendizado_supervisionado/regressao/minimos_quadrados_ordinario.py
+++ aprendizado_supervisionado/regressao/minimos_quadrados_ordinario.py
@@ -23,45 +23,46 @@ from random import uniform
 def minimos_quadrados(X, Y):
     '''Implementa método mínimos quadrados.'''
 
-    # encontar o coeficiente angular m
+    # encontrar o coeficiente angular m
     n = len(X)
-    sumxy = sum([x * y for x,y in zip(X,Y)])
+    sumxy = sum([x * y for x,y in zip(X, Y)])
     sumx = sum(X)
     sumy = sum(Y)
-    sumx2 = sum([x*x for x in X])
-    
+    sumx2 = sum([x * x for x in X])
+
     m = (n * sumxy - sumx * sumy) / (n * sumx2 - sumx**2)
-    # encontrar o coeficiente linear b    
+
+    # encontrar o coeficiente linear b
     b = (sumy - m * sumx) / n
 
-    return m, b
- 
-def gerar_vetores():
-    '''Gerar vetores X e Y.'''
+    return m,b
 
+def gerar_vetores():
+    '''Gerar vetores de X e Y.'''
     m, b = 2, 5
-    X = [x for x in range(10)]    
-    Y = [m * x + b + uniform(0,2) for x in X]
+    X = [x for x in range(10)]
+    Y = [m * x + b + uniform(0, 2) for x in X]
+
     return X, Y
 
 def main():
     '''Executa função principal.'''
-    
+
     X, Y = gerar_vetores()
-    
-    # estimatar valores
+
+    # estimar valores de Y
     m, b = minimos_quadrados(X, Y)
     Y_pred = [m * x + b for x in X]
-    
-    # avaliar modelo
-    erro = rmse(Y,Y_pred)
-    print(erro)
-    print(m,b)
 
-    # plotar gráfico
-    gtrue = plt.scatter(X,Y)
-    gpred = plt.scatter(X,Y_pred)
-    plt.legend((gtrue, gpred),('Y Real', 'Y Estimado'))
+    # avaliar modelo
+    erro = rmse(Y, Y_pred)
+    print(erro)
+    print(m, b)
+
+    # plotar gráficos
+    gtrue = plt.scatter(X, Y)
+    gpred = plt.scatter(X, Y_pred)
+    plt.legend((gtrue, gpred), ('Y Real', 'Y Estimado'))
     plt.xlabel('Valores de X')
     plt.ylabel('Valores de Y')
     plt.show()
